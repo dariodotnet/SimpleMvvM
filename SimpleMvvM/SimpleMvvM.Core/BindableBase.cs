489c92a633ef8ac7895dd80c49d15e7c076600bb
Dario Alonso Cerezo <dariovillar@hotmail.com>
2017-07-13 22:10:14 +0200

    Commit inicial. Creado el proyecto base con la implementación de Comandos y propiedades que notifican cambios.

---
+++ SimpleMvvM/SimpleMvvM.Core/BindableBase.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleMvvM.Core
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (Equals(storage, value)) return false;
+            storage = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ViewModelBase : BindableBase
+    {
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+    }
+}
---
+++ SimpleMvvM/SimpleMvvM.Core/DelegateCommandAsync.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleMvvM.Core
+{
+    public class DelegateCommandAsync : ICommand
+    {
+        private readonly Func<Task<bool>> _canExecute;
+        private readonly Func<Task> _execute;
+
+        public DelegateCommandAsync(Func<Task> execute) : this(execute, null)
+        {
+        }
+
+        public DelegateCommandAsync(Func<Task> execute, Func<Task<bool>> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute().Result;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var tmpHandle = CanExecuteChanged;
+            tmpHandle?.Invoke(this, new EventArgs());
+        }
+    }
+
+    public class DelegateCommandAsync<T> : ICommand
+    {
+        private readonly Func<T, Task<bool>> _canExecute;
+        private readonly Func<T, Task> _execute;
+
+        public DelegateCommandAsync(Func<T, Task> execute) : this(execute, null)
+        {
+        }
+
+        public DelegateCommandAsync(Func<T, Task> execute, Func<T, Task<bool>> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter).Result;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var tmpHandle = CanExecuteChanged;
+            tmpHandle?.Invoke(this, new EventArgs());
+        }
+    }
+}
